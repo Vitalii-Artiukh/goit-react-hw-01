d39db2cb54e2ac01ca649387c5dac7772eba801a
Vitalii Artiukh <radial_car@yahoo.com>
2024-09-24 21:18:37 +0300

    frendListItem-css

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,15 +1,15 @@
-import styles from '../FriendList/FriendList.module.css';
+import styles from './friendListItem.module.css';
 import clsx from 'clsx';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
       <img src={avatar} alt={'Avatar ' + name} width="80" />
-      <p className={styles.name}>{name}</p>
+      <p>{name}</p>
       {isOnline ? (
-        <p className={styles.online}>Online</p>
+        <p className={clsx(styles.online)}>Online</p>
       ) : (
-        <p className={styles.ofline}>Ofline</p>
+        <p className={clsx(styles.ofline)}>Ofline</p>
       )}
     </div>
   );
